a78ae0f439357a4a57694f3f8839e1f97c1b65a4
muxika21 <muxika.el@gmail.com>
2024-07-08 00:58:32 +0800

    update app

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up the database connection
 const connection = mysql.createConnection({
@@ -17,26 +14,9 @@ const connection = mysql.createConnection({
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const { updated } = req.query;
-  res.render('index', { updated });
-});
-
-app.post('/add', (req, res) => {
-  const { staff_name, model, serial_number } = req.body;
-  connection.query(
-    'INSERT INTO cicd_pipeline (staff_name, model, serial_number) VALUES (?, ?, ?)',
-    [staff_name, model, serial_number],
-    (error, results) => {
-      if (error) throw error;
-      res.redirect('/?updated=true');
-    }
-  );
-});
-
-app.get('/search', (req, res) => {
   connection.query('SELECT staff_name, model, serial_number FROM cicd_pipeline', (error, results) => {
     if (error) throw error;
-    res.render('asset_list', { users: results });
+    res.render('index', { users: results });
   });
 });
 
