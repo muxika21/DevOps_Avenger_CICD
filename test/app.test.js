6f892ad89d4b792e38ccd3f487803e533517696e
muxika21 <muxika.el@gmail.com>
2024-07-08 23:30:52 +0800

    Revert to basic test setup without nyc

--- test/app.test.js
+++ test/app.test.js
@@ -1,46 +1,25 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import router from './routes/index.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, '../', 'public')));
-
-const PORT = 3000;
-
-router(app);
-app.get('/', (req, res) => {
-  res.send('<h1> Hello World </h1>');
-});
-
-app.use((req, res, next) => {
-  const error = new Error();
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  if (error.status === 404) {
-    res.status(404);
-    return res.render('errors/404');
-  }
-
-  res.status(500);
-  res.render('error/500');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
-});
-
-export default app;
+{
+  "name": "devops-avengers-cicd-app",
+  "version": "1.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "test": "mocha test/app.test.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "ejs": "^3.1.9",
+    "mysql2": "^2.3.3"
+  },
+  "devDependencies": {
+    "chai": "^4.3.7",
+    "chai-http": "^4.3.0",
+    "mocha": "^10.2.0"
+  },
+  "engines": {
+    "node": "v20.x"
+  },
+  "author": "",
+  "license": "ISC",
+  "type": "module"  // Enable ES Module support if you choose to use ES Modules
+}
