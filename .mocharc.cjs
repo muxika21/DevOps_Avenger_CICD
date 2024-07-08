fca6b11ad2d7e0e66d7df9d1eff5a111b5e9e0be
muxika21 <muxika.el@gmail.com>
2024-07-08 23:37:19 +0800

    test

---
+++ .mocharc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    require: '@babel/register',
+    extension: ['js'],
+    spec: 'test/**/*.test.js'
+  };
+  
--- test/app.test.js
+++ test/app.test.js
@@ -1,25 +1,15 @@
-{
-  "name": "devops-avengers-cicd-app",
-  "version": "1.0.0",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js",
-    "test": "mocha test/app.test.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1",
-    "ejs": "^3.1.9",
-    "mysql2": "^2.3.3"
-  },
-  "devDependencies": {
-    "chai": "^4.3.7",
-    "chai-http": "^4.3.0",
-    "mocha": "^10.2.0"
-  },
-  "engines": {
-    "node": "v20.x"
-  },
-  "author": "",
-  "license": "ISC",
-  "type": "module"  // Enable ES Module support if you choose to use ES Modules
-}
+import chai from 'chai';
+import request from 'supertest';
+import app from '../src/index.js';  // Import the app object
+
+const { expect } = chai;
+
+describe('GET /', () => {
+  it('should return a 200 status code and "Hello World" message', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect('<h1> Hello World </h1>', done);
+  });
+});
